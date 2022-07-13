62559b1ac46d55765f688aa3c8164d4fa9670e27
Artem Derevenets <derevenetsartyom@gmail.com>
2022-07-13 22:06:52 +0300

    Better structure - API v1 support

--- app/main.py
+++ app/main.py
@@ -1,23 +1,22 @@
 from pathlib import Path
-from typing import Any, Optional
 
-from fastapi import APIRouter, Depends, FastAPI, HTTPException, Query, Request
+from fastapi import APIRouter, Depends, FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 
 from app import crud
-from app import deps
-from app.schemas.recipe import Recipe, RecipeCreate, RecipeSearchResults
+from app.api import deps
+from app.api.api_v1.api import api_router
+from app.core.config import settings
 
 BASE_PATH = Path(__file__).resolve().parent
 TEMPLATES = Jinja2Templates(directory=str(BASE_PATH / "templates"))
 
 app = FastAPI(title="Recipe API")
+root_router = APIRouter()
 
-api_router = APIRouter()
 
-
-@api_router.get("/", status_code=200)
+@root_router.get("/", status_code=200)
 def root(request: Request, db: Session = Depends(deps.get_db)) -> dict:
     """Root GET."""
 
@@ -25,43 +24,8 @@ def root(request: Request, db: Session = Depends(deps.get_db)) -> dict:
     return TEMPLATES.TemplateResponse("index.html", {"request": request, "recipes": recipes})
 
 
-@api_router.get("/recipe/{recipe_id}", status_code=200, response_model=Recipe)
-def fetch_recipe(*, recipe_id: int, db: Session = Depends(deps.get_db)) -> Any:
-    """Fetch a single recipe by ID."""
-
-    result = crud.recipe.get(db=db, _id=recipe_id)
-    if not result:
-        # the exception is raised, not returned - you will get a validation error otherwise.
-        raise HTTPException(status_code=404, detail=f"Recipe with ID {recipe_id} not found")
-
-    return result
-
-
-@api_router.get("/search/", status_code=200, response_model=RecipeSearchResults)
-def search_recipes(
-    *,
-    keyword: Optional[str] = Query(None, min_length=3, example="chicken"),
-    max_results: Optional[int] = 10,
-    db: Session = Depends(deps.get_db)
-) -> dict:
-    """Search for recipes based on label keyword."""
-
-    recipes = crud.recipe.get_multi(db=db, limit=max_results)
-    if not keyword:
-        return {"results": recipes}
-
-    results = filter(lambda recipe: keyword.lower() in recipe.label.lower(), recipes)
-    return {"results": list(results)[:max_results]}
-
-
-@api_router.post("/recipe/", status_code=201, response_model=Recipe)
-def create_recipe(*, recipe_in: RecipeCreate, db: Session = Depends(deps.get_db)) -> dict:
-    """Create a new recipe (in memory only)."""
-
-    return crud.recipe.create(db=db, obj_in=recipe_in)
-
-
-app.include_router(api_router)
+app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(root_router)
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
