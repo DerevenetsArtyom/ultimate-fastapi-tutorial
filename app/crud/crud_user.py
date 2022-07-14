ecadc236ba446e4bb56219b5447323989e41cede
Artem Derevenets <derevenetsartyom@gmail.com>
2022-07-14 15:29:42 +0300

    Override `CRUDUser.create()` to hash password

--- app/crud/crud_user.py
+++ app/crud/crud_user.py
@@ -2,6 +2,7 @@ from typing import Any, Dict, Optional, Union
 
 from sqlalchemy.orm import Session
 
+from app.core.security import get_password_hash
 from app.crud.base import CRUDBase
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
@@ -10,6 +11,15 @@ from app.schemas.user import UserCreate, UserUpdate
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
     def get_by_email(self, db: Session, *, email: str) -> Optional[User]:
         return db.query(User).filter(User.email == email).first()
+
+    def create(self, db: Session, *, obj_in: UserCreate) -> User:
+        create_data = obj_in.dict()
+        create_data.pop("password")
+        db_obj = User(**create_data)
+        db_obj.hashed_password = get_password_hash(obj_in.password)
+        db.add(db_obj)
+        db.commit()
+        return db_obj
 
     def update(self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]) -> User:
         if isinstance(obj_in, dict):
