4ee1d435a289a44c1e1069acb0791b89c7a7f147
Artem Derevenets <derevenetsartyom@gmail.com>
2022-07-15 20:42:57 +0300

    Add `get_current_active_superuser()` dependency

--- app/api/deps.py
+++ app/api/deps.py
@@ -4,6 +4,7 @@ from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
 from sqlalchemy.orm import Session
 
+from app import crud
 from app.core.auth import oauth2_scheme
 from app.core.config import settings
 from app.db.session import SessionLocal
@@ -45,3 +46,10 @@ def get_current_user(db: Session = Depends(get_db), token: str = Depends(oauth2_
         raise credentials_exception
 
     return user
+
+
+def get_current_active_superuser(current_user: User = Depends(get_current_user)) -> User:
+    if not crud.user.is_superuser(current_user):
+        raise HTTPException(status_code=400, detail="The user doesn't have enough privileges")
+
+    return current_user
